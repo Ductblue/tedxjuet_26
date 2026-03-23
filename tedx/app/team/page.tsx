126f549b181ffa2a22286cc781625c9d38b83c19
Ductblue <tripathishreyansh88@gmail.com>
2026-03-23 14:07:24 +0530

    Added final photos

--- tedx/app/team/page.tsx
+++ tedx/app/team/page.tsx
@@ -32,7 +32,7 @@ const teamMembers = [
     role: "Organizer",
     department: "Organization",
     bio: "Leading the TEDxJUET event with passion and dedication",
-    image: null
+    image: "/team/member1.jpeg"
   },
   {
     id: 2,
